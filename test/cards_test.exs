2f2c7b0ba5ca160ee81a3544ead68799b3bb2a89
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-11-27 06:48:24 +0100

    explore both doctests and case tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,16 @@
 defmodule CardsTest do
   use ExUnit.Case
-  doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  doctest Cards # will find doctest in the documentation !!!
+
+  test "shuffled deck is different" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
   end
+
+  test "another way... shuffled deck is different" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck) 
+  end
+
 end
