0ae3bd24cbb729ad1bc98fa1413a658bdd7d740e
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2023-11-27 06:47:22 +0100

    add some documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,22 +1,14 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   
+  @moduledoc """
+    
+    A card games related module
+    
+  """
+
+  @doc """ 
+    Returns a list of strings that represent a deck of cards
+  """
   def create_deck do
     suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -26,24 +18,49 @@ defmodule Cards do
       end
   end
   
+ 
+  @doc """ 
+    Takes a `deck` (a list of strings) and returns a randomly shuffled copy of it.
+  """
   def shuffle(deck) do 
     Enum.shuffle(deck)
   end
 
+  
+  @doc """ 
+    Deals a hand from a `deck` (a list of strings) and `n_cards` (an int) 
+    Returns the new `deck` (with `n_cards` less) and the hand (`n_cards` long) 
+  """
   def deal(deck, n_cards) do
     Enum.split(deck, n_cards)
   end
 
+  
+  @doc """ 
+    Determines whether a `deck` contains a `card` or not. 
 
+  
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
   def save(deck, filename) do 
     binary = :erlang.term_to_binary(deck)
+  @doc """
+    Allows to save `cards` to disk. Takes the `cards` and the `filename` as args.   
+  """
+  def save(cards, filename) do 
+    binary = :erlang.term_to_binary(cards)
     File.write(filename, binary)
   end
 
+  
+  @doc """
+    Allows to load cards from disk. 
+    Takes the `filename` as arg and return the cards. 
+  """
   def load(cards_path) do
     case File.read(cards_path) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -51,6 +68,14 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    A utility function that, given a number of cards, will:
+      - create a deck 
+      - shuffle it 
+      - and deal a hand
+
+
+  """
   def create_hand(n_cards) do 
     Cards.create_deck()
     |> Cards.shuffle 
